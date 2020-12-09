5083be6c2aa6bf623233e265cbbcbf669d1c21d0
aaronalayo <aaro0186@stud.kea.dk>
2020-12-09 16:00:32 +0100

    Modified ftp config

--- app/src/ftp.js
+++ app/src/ftp.js
@@ -4,14 +4,6 @@ const path = require('path');
 const dotenvPath = path.join(__dirname, '..', '.env');
 require('dotenv').config({path: dotenvPath});
 
-
-// var config = {
-//   host: "94.231.99.28",
-//   port: 21,
-//   username: "EksternTest",
-//   password: "h242svgw",
-//   type: "ftp",
-// };
 var config = {
   host: process.env.FTPHOST,
   port: process.env.FTPPORT,
