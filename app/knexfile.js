0518c2ae65085561d925d96ffda213d3b1aaf11c
aaronalayo <aaro0186@stud.kea.dk>
2021-01-16 12:00:40 +0100

    Test db

--- app/knexfile.js
+++ app/knexfile.js
@@ -9,13 +9,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+
 module.exports = {
   development: {
     client: "pg",
 
     connection: {
       user: process.env.PGUSER,
-      host: process.env.PGHOST,
+      host: process.env.PGHOST || process.env.CLOUD_SQL_CONNECTION_NAME ,
       database: process.env.PGDATABASE,
       password: process.env.PGPASSWORD,
       port: process.env.PGPORT,
