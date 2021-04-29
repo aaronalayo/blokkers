da819fd48415d3da509675e70621fe56d4b28331
aaronalayo <aaron.aa@me.com>
2021-04-29 11:55:23 +0200

    Commented mongo

--- app/src/app.js
+++ app/src/app.js
@@ -49,46 +49,46 @@ dotenv.config();
 //   res.locals.session = req.session;
 //   next();
 // })
-async function main() {
+// async function main() {
 
-  try {
+//   try {
  
-      const mongoDbUrl = process.env.DB_HOST;
-      // mongoCon();
-      // Use new url parser and unified topology to fix deprecation warning
-      MongoDBStore = mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(async function() {
-          console.log(`Successfully connected to MongoDB database at ${mongoDbUrl}!`);
-      }).catch(function(error) {
-          console.log(`Error whilst connecting to MongoDB database at ${mongoDbUrl}! ${error}`);
-      });
-      mongoose.set('useCreateIndex', true); // Fixes deprecation warning
-      // mongoose.Collection('posters');
-  } catch (err) {
-      console.log(`Error whilst doing database setup! ${err}`);
-  }
-  try {
-    app.use(cookieParser());
-    app.use(session({
-      saveUninitialized: true, // don't create session until something stored
-      resave: false, //don't save session if unmodified
-      store: store,
-      secret: process.env.SESSION_SECRET,
-      unset: 'destroy',
-      name: 'blokkers',
-      domain: ".blokkers.dk",
-      cookie: {
-          maxAge: 1000 * 60 * 60 * 24 * 15, // 2 week
-          httpOnly: false,
-          secure: false // TODO: Update this on production     
-         }
-    }));
+//       const mongoDbUrl = process.env.DB_HOST;
+//       // mongoCon();
+//       // Use new url parser and unified topology to fix deprecation warning
+//       MongoDBStore = mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(async function() {
+//           console.log(`Successfully connected to MongoDB database at ${mongoDbUrl}!`);
+//       }).catch(function(error) {
+//           console.log(`Error whilst connecting to MongoDB database at ${mongoDbUrl}! ${error}`);
+//       });
+//       mongoose.set('useCreateIndex', true); // Fixes deprecation warning
+//       // mongoose.Collection('posters');
+//   } catch (err) {
+//       console.log(`Error whilst doing database setup! ${err}`);
+//   }
+//   try {
+//     app.use(cookieParser());
+//     app.use(session({
+//       saveUninitialized: true, // don't create session until something stored
+//       resave: false, //don't save session if unmodified
+//       store: store,
+//       secret: process.env.SESSION_SECRET,
+//       unset: 'destroy',
+//       name: 'blokkers',
+//       domain: ".blokkers.dk",
+//       cookie: {
+//           maxAge: 1000 * 60 * 60 * 24 * 15, // 2 week
+//           httpOnly: false,
+//           secure: false // TODO: Update this on production     
+//          }
+//     }));
     
-      console.log("Sessions successfully initialized!");
-  } catch (err) {
-      console.log(`Error setting up a mongo session store! ${err}`);
-  }
-}
-main();
+//       console.log("Sessions successfully initialized!");
+//   } catch (err) {
+//       console.log(`Error setting up a mongo session store! ${err}`);
+//   }
+// }
+// main();
 
 const server = require("http").createServer(app);
 
